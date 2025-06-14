5fc853d57ec879bc5f76b684ffba10cc9b04de37
Айбек Шамшиев <ashamshievqw@gmail.com>
2025-06-15 02:02:38 +0600

    add send username

--- src/telegram.ts
+++ src/telegram.ts
@@ -5,7 +5,6 @@ export function setupTelegram(): void {
   tg.expand()
   // Use dark header and hide the default main button for a clean look
   try {
-    tg.setHeaderColor('#000000')
     tg.MainButton.hide()
   } catch (e) {
     console.error('Telegram WebApp API error', e)
