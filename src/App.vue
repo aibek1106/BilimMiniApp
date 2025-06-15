1105575e539554e1dfaa5ad5b455dbb5d321b49a
aibek1106 <92970752+aibek1106@users.noreply.github.com>
2025-06-15 11:04:24 +0600

    Revert navigation back to bottom

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="app">
-    <TopNav />
+    <BottomNav />
     <router-view />
   </div>
 </template>
 
 <script setup lang="ts">
 import { watch } from 'vue'
-import TopNav from './components/TopNav.vue'
+import BottomNav from './components/BottomNav.vue'
 import { setHeaderColor } from './telegram'
 import { theme } from './theme'
 
@@ -29,15 +29,16 @@ watch(
   position: relative;
   height: 100%;
   box-sizing: border-box;
-  padding-top: calc(
-    var(
-        --tg-content-safe-area-inset-top,
-        var(--tg-safe-area-inset-top, env(safe-area-inset-top))
-      ) + 5rem
+  padding-bottom: calc(
+    6.5rem +
+      var(
+        --tg-content-safe-area-inset-bottom,
+        var(--tg-safe-area-inset-bottom, env(safe-area-inset-bottom))
+      )
   );
-  padding-bottom: var(
-    --tg-content-safe-area-inset-bottom,
-    var(--tg-safe-area-inset-bottom, env(safe-area-inset-bottom))
+  padding-top: var(
+    --tg-content-safe-area-inset-top,
+    var(--tg-safe-area-inset-top, env(safe-area-inset-top))
   );
   overflow-y: auto;
 }
